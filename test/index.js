78aac887df1a2de78391d156377a4de971f1d49d
Robert Kowalski <rok@kowalski.gd>
2014-08-15 19:15:28 +0200

    fix test that was depending on the current date

--- test/index.js
+++ test/index.js
@@ -3,17 +3,25 @@ var test = require("tap").test
   , request = require("request")
   , ai = require("../")
   , port = 1337
+  , mockRes = require("./fixtures/github-response.json")
 
 var server
 
 test("setup", function (t) {
+  var threeDaysAgo = new Date(new Date() - (1000 * 60 * 60 * 24 * 3))
+  Object.keys(mockRes.items).forEach(function (i) {
+    var item = mockRes.items[i]
+    if (item.url === "https://api.github.com/repos/npm/npm/issues/4714")
+      item.updated_at = threeDaysAgo.toISOString()
+  });
+
   hock.createHock(port, function (err, hockServer) {
     server = hockServer
 
     hockServer
       .get("/some/url")
       .many()
-      .reply(200, require("./fixtures/github-response.json"))
+      .reply(200, mockRes)
 
     t.end()
   })
